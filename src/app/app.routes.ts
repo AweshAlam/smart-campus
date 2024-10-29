24bfd29dbadccb85493e90bf65baf6d883949207
aftab <220101120107@cutm.ac.in>
2024-10-29 22:51:10 +0530

    login done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { StudentRegistrationComponent } from './student/student-registration/stu
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { AdminLoginComponent } from './login/admin-login/admin-login.component';
 import { StudentDashboardComponent } from './student/student-dashboard/student-dashboard.component';
+import { AuthGuard } from './auth.guard';
 export const routes: Routes = [
   { path: '', redirectTo:'home', pathMatch:'full'},
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin/student/register', component: StudentRegistrationComponent },
-  { path: 'admin', component: AdminDashboardComponent },
-  { path: 'admin-login', component:AdminLoginComponent},
-  { path: 'student', component: StudentDashboardComponent},
+  { path: 'admin', component: AdminDashboardComponent,canActivate: [AuthGuard], data: { role: 'ROLE_ADMIN' } },
+  { path: 'admin-login', component:AdminLoginComponent,},
+  { path: 'student', component: StudentDashboardComponent,canActivate: [AuthGuard], data: { role: 'ROLE_STUDENT'}},
   { path: '**', redirectTo: 'home', pathMatch:'full' }
 ];
